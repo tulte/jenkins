f11153b639075cc9faf1000fa9b9fbed5c2d9d0e
jenkins <tobias.mueller@check24.de>
2020-08-13 10:14:40 +0200

    1

--- test.py
+++ test.py
@@ -18,12 +18,6 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World 1!\n')
 
-    def test_hello_name(self):
-        name = 'Simon'
-        rv = self.app.get(f'/hello/{name}')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
-
 if __name__ == '__main__':
     ############# Add these lines #############
     import xmlrunner
