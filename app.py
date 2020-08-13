0befb5f7ba1b146982b2b9b7537ebf3c82279068
jenkins <tobias.mueller@check24.de>
2020-08-13 10:13:33 +0200

    helloworld 1

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World 1!\n'
 
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Why Hello !\n'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
--- test.py
+++ test.py
@@ -11,12 +11,12 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello World 1!\n')
 
     def test_hello_hello(self):
         rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello World 1!\n')
 
     def test_hello_name(self):
         name = 'Simon'
